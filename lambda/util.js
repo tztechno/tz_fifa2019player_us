d68738dd4eebf811043d400d3232ba8ae0348cb6
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2019-07-31 10:49:15 +0000

    updated lambda/util.js.

--- lambda/util.js
+++ lambda/util.js
@@ -10,9 +10,20 @@ module.exports.getS3PreSignedUrl = function getS3PreSignedUrl(s3ObjectKey) {
     const s3PreSignedUrl = s3SigV4Client.getSignedUrl('getObject', {
         Bucket: bucketName,
         Key: s3ObjectKey,
-        Expires: 60*1 // the Expires is capped for 1 minute
+        Expires: 60 * 1 // the Expires is capped for 1 minute
     });
     console.log(`Util.s3PreSignedUrl: ${s3ObjectKey} URL ${s3PreSignedUrl}`);
     return s3PreSignedUrl;
 
+}
+
+
+// util.js へ以下を追加
+module.exports.isAPLSupported = function isAPLSupported(request) {
+    return request &&
+        request.context &&
+        request.context.System &&
+        request.context.System.device &&
+        request.context.System.device.supportedInterfaces &&
+        request.context.System.device.supportedInterfaces['Alexa.Presentation.APL'];
 }
